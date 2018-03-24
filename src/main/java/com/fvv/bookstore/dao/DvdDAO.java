6e7a787c6ec54f3ce2a284849c0a1fbe79107c0f
Fatima Vasquez <fvv.vasquez@gmail.com>
2018-03-24 19:41:50 -0300

    deleting classes

--- src/main/java/com/fvv/bookstore/dao/DvdDAO.java
+++ src/main/java/com/fvv/bookstore/dao/DvdDAO.java
@@ -11,7 +11,7 @@ import com.fvv.bookstore.bean.MovieDvd;
  * @author Fatima Vasquez
  * @version 1.0
  */
-public interface MovieDvdDAO {
+public interface DvdDAO {
 
 	/**
 	 * Add a movieDvd at the bookstore in the database.
