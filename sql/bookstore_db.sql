45340d2947aba77679992a8845ca5e6287914faf
Fatima Vasquez <fvv.vasquez@gmail.com>
2018-04-14 19:10:28 -0300

    Updating sql

--- sql/bookstore_db.sql
+++ sql/bookstore_db.sql
@@ -25,13 +25,14 @@ CREATE TABLE `tb_book` (
   `book_publication_year` int(11) NOT NULL,
   `book_edition_number` int(11) NOT NULL,
   `book_author` varchar(255) NOT NULL,
-  `book_price` double NOT NULL,
+  `book_unit_price` double NOT NULL,
   `book_isbn` int(11) NOT NULL,
   `book_publisher` varchar(255) NOT NULL,
   `book_genre` varchar(255) NOT NULL,
+  `book_stock_qty` int(11) NOT NULL,
   `book_modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=147 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=146 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `tb_cellphone` */
 
@@ -40,10 +41,11 @@ DROP TABLE IF EXISTS `tb_cellphone`;
 CREATE TABLE `tb_cellphone` (
   `cel_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `cel_brand` varchar(255) NOT NULL,
-  `cel_price` double NOT NULL,
+  `cel_unit_price` double NOT NULL,
   `cel_warranty` int(11) NOT NULL,
   `cel_storage_memory` int(11) NOT NULL,
   `cel_camera_pixels` int(11) NOT NULL,
+  `cel_stock_qty` int(11) NOT NULL,
   `cel_modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`cel_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
@@ -71,15 +73,16 @@ CREATE TABLE `tb_dvd` (
   `dvd_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `dvd_title` varchar(255) NOT NULL,
   `dvd_total_duration` int(11) NOT NULL,
-  `dvd_price` double NOT NULL,
+  `dvd_unit_price` double NOT NULL,
   `dvd_genre` varchar(255) NOT NULL,
   `dvd_release_year` int(11) NOT NULL,
   `dvd_code` int(11) NOT NULL,
   `dvd_movie_director` varchar(255) DEFAULT NULL,
   `dvd_show_artist` varchar(255) DEFAULT NULL,
+  `dvd_stock_qty` int(11) NOT NULL,
   `dvd_modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`dvd_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `tb_employee` */
 
@@ -93,8 +96,9 @@ CREATE TABLE `tb_employee` (
   `emp_cpf` varchar(255) NOT NULL,
   `emp_position` varchar(255) NOT NULL,
   `emp_salary` double NOT NULL,
+  `emp_modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`emp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `tb_laptop` */
 
@@ -103,11 +107,12 @@ DROP TABLE IF EXISTS `tb_laptop`;
 CREATE TABLE `tb_laptop` (
   `pc_id` int(11) NOT NULL AUTO_INCREMENT,
   `pc_brand` varchar(255) NOT NULL,
-  `pc_price` double NOT NULL,
+  `pc_unit_price` double NOT NULL,
   `pc_warranty` int(11) NOT NULL,
   `pc_ram_size` int(11) NOT NULL,
   `pc_hd_size` double NOT NULL,
   `pc_processor` varchar(255) NOT NULL,
+  `pc_stock_qty` int(11) NOT NULL,
   `pc_modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`pc_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
@@ -123,10 +128,11 @@ CREATE TABLE `tb_magazine` (
   `mag_genre` varchar(255) NOT NULL,
   `mag_publication_date` date NOT NULL,
   `mag_publisher` varchar(255) NOT NULL,
-  `mag_price` double NOT NULL,
+  `mag_unit_price` double NOT NULL,
+  `mag_stock_qty` int(11) NOT NULL,
   `mag_modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`mag_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
